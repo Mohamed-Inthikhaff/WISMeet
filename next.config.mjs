199a336455ff1b3a65fcbc15e68e875e63716001
Mohamed Inthikhaff <mohamed.inthikhaff@wismortgages.co.uk>
2025-07-30 13:56:44 +0100

    Enhance: Add environment variables for Clerk and Stream API keys in next.config.mjs; remove outdated PARTICIPANT_VISIBILITY_FIX.md documentation.

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,13 @@ const nextConfig = {
   images: {
     domains: ['images.clerk.dev'],
   },
+  env: {
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: 'pk_test_ZGVjaWRpbmctZWFyd2lnLTI1LmNsZXJrLmFjY291bnRzLmRldiQ',
+    CLERK_SECRET_KEY: 'sk_test_i9fzVoV8TY8UvIAb3MwF3BoMY73APhfNJs7hOuNl48',
+    NEXT_PUBLIC_STREAM_API_KEY: 'pmx7dhj5kn7j',
+    STREAM_SECRET_KEY: '3jj98de3nez3v48zyaub4rzv72wgrhrfz2tabzbn29ntkkf628dxwttdgqphb3cg',
+    NEXT_PUBLIC_BASE_URL: 'http://localhost:3000',
+  },
 };
 
 export default nextConfig;
